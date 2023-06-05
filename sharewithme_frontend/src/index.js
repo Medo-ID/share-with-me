e7b4713916e95302f0a402487803f98507eae984
Medo-ID <midaghdour@gmail.com>
2023-06-05 11:54:03 +0100

    first commit

---
+++ sharewithme_frontend/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client';
+import {BrowserRouter as Router} from 'react-router-dom'
+
+import App from './App'
+import './index.css'
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+
+    <Router>
+        <App />
+    </Router>
+
+);
